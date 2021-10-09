b63c8b0b78db1d4e3ca2e2b6200b9a729a04d28d
edgar <edgarpm1820@gmail.com>
2021-10-09 00:46:03 -0700

    Second try at making the project work with heroku

--- CST438project2/settings.py
+++ CST438project2/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s#clw9j09vtg7mj13p5c2(r8tk5$c_*@8o!f6w9q8mor9(20g2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cstproject2.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CST438project2.wsgi --log-file -
