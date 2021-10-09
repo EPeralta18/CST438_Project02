606174f8a3e4115476b6de457a9f8b7315bae4c4
edgar <edgarpm1820@gmail.com>
2021-10-08 19:22:50 -0700

    Added file to work with heroku

--- my_django_app/my_django_app/settings.py
+++ my_django_app/my_django_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0&(rj45ap8vmo463=fc555loj7f-(4ea2fvu#phc6xac_=mvvx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cstproject2.herokuapp.com', 'localhost']
 
 
 # Application definition
