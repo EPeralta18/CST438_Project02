562c014272297b335d229b3dfb32417056f67b4b
edgar <edgarpm1820@gmail.com>
2021-10-09 00:38:17 -0700

    Fresh start

---
+++ home/static/home/script.js
@@ -0,0 +1,7 @@
+console.log("waassup");
+document.getElementById('imgShow').src = 'https://picsum.photos/'+(200+rand())+'/' + (300 + rand()) +'?random=1';}
+
+function rand()
+{
+return Math.floor(Math.random() * 90)
+};
