2b695e7ab208ae83e20b4a4ee3f21350d557c779
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2021-10-19 23:12:25 +0530

    Added methods for database operations
    addItem method added
    addItemToUserWishlist method added
    removeItemFromWishlist method added

--- Firebase.py
+++ Firebase.py
@@ -27,14 +27,14 @@ class Firebase:
       for key in users:
         if users[key]['username'] == username:
           return users[key], key
-      return Null
+      return None
 
     def getUserWishlist(self, username):
       return self.database.child('wishlist').get().val()[username]
     
     def getItem(self, index):
       items = self.getItems()
-      return [items[i] for i in index]
+      return [items[index[key]] for key in index]
 
     def addUser(self, username, password):
       data = {
@@ -51,8 +51,34 @@ class Firebase:
     def setAdminStatus(self, username, adminStatus):
       user, key = self.getUser(username)
       self.database.child('users').child(key).update({"adminStatus" : adminStatus})
-      print()
+    
+    def addItem(self, name, itemLink, image='No Image', website='No website'):
+      items = self.getItems()
+      data = {
+        "name" : name,
+        "link" : itemLink,
+        "image": image,
+        "website": website, 
+      }
+      self.database.child('items').push(data)
+      return self.getItemKey(name, itemLink, image, website)
 
+    def addItemToUserWishlist(self, username, itemKey):
+      self.database.child('wishlist').child(username).push(itemKey)
 
+    def getItemKey(self, name, link, image='No Image', website='No website'):
+      items = self.getItems()
+      for key in items:
+        if items[key]["name"] == name and items[key]["link"] == link and items[key]["image"] == image and items[key]["website"] == website:
+          return key
+      return None
+    
+    def removeItem(self, key):
+      self.database.child('items').child(key).remove()
 
-firebase = Firebase()
+    def removeItemFromWishlist(self, username, itemKey):
+      wishlist = self.getUserWishlist(username)
+      for i in wishlist:
+        if wishlist[i] == itemKey:
+          self.database.child('wishlist').child(username).child(i).remove()
+
