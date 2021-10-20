32636556fbb7e237332380f44aba1830b645ef4f
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2021-10-20 05:54:08 +0530

    Fixed static files directory

--- home/static/home/script.js
+++
@@ -1,7 +0,0 @@
-console.log("waassup");
-document.getElementById('imgShow').src = 'https://picsum.photos/'+(200+rand())+'/' + (300 + rand()) +'?random=1';
-
-function rand()
-{
-return Math.floor(Math.random() * 90)
-};
