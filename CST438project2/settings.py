39716ed18bb7a2ffda4fdc8a3d10b03efeb78b8c
Edgar Peralta <53790908+EPeralta18@users.noreply.github.com>
2021-10-23 17:28:31 -0700

    Update settings.py

--- CST438project2/settings.py
+++ CST438project2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s#clw9j09vtg7mj13p5c2(r8tk5$c_*@8o!f6w9q8mor9(20g2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cstproject2.herokuapp.com', '127.0.0.1']
 
